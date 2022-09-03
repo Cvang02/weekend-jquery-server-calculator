0209bbd8dac3d8e5c68a1e7cecd8d8fc4f1f5986
Chameng Vang <Chameng.vang@outlook.com>
2022-09-02 23:35:13 -0500

    number input for POST/GET is working as intended. Numbers are being added to collection and message is being recieved.

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -1,2 +1,30 @@
 // This is just to make sure client.js is running on the browser for visual.  
 console.log('This means that client.js is working!!!');
+
+$(document).ready(onReady);
+console.log('jquery is loaded'); // this is just to make sure ready DOC is loading.
+
+function onReady () {
+    $('#equalButton').on('click', numberInput);
+} // end of onReady-function. 
+
+// For this function, we want to send the input numbers 
+// and operator to the server side. 
+// We will then recieved a message indicating that server has recieved 
+// and collected information. 
+function numberInput () {
+    let number1 = $('#firstNumberInput').val();
+    let number2 = $('#secondNumberInput').val();
+
+    $.ajax({
+        type: 'POST',
+        url: '/calculation',
+        data: {number1, number2}
+    }).then(function(response) {
+        console.log('info collected');
+    })
+
+    $('input').val('');
+
+} //end of numberInput-function. 
+
--- server/server.js
+++ server/server.js
@@ -4,13 +4,24 @@ const app = express();
 const PORT = 5000;
 app.use(express.static('server/public'));
 const bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded({extended:true}))
 
 // This is just to make sure server is running when connected for visual on terminal. 
-console.log('server.js is working and working');
+console.log('server.js is working and running!!!');
 
+let calculationHistory = [];
 
+// GET AND POST ROUTES GOES HERE. 
+app.post('/calculation', (req, res) => {
+  calculationHistory.push(req.body);
+  console.log('req.body is:', req.body);
+  console.log(calculationHistory);
+  res.sendStatus(201);
+})
 
-
+app.get('/calculation', (req, res) => {
+  res.send(calculationHistory);
+})
 
 
 
